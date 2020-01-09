5684c27be8aac34f640acc9b2e2a327cccfbf4ed
fusytc <2321959010@qq.com>
2020-01-09 19:56:27 +0800

    zipmap

--- src/zipmap.h
+++ src/zipmap.h
@@ -35,14 +35,22 @@
 #ifndef _ZIPMAP_H
 #define _ZIPMAP_H
 
+// 创建zipmap
 unsigned char *zipmapNew(void);
+// 如果存在指定key对应的元素，就修改指定key对应的value值，如果不存在则新增元素
 unsigned char *zipmapSet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char *val, unsigned int vlen, int *update);
+// 删除元素
 unsigned char *zipmapDel(unsigned char *zm, unsigned char *key, unsigned int klen, int *deleted);
+// 获取元素的首地址
 unsigned char *zipmapRewind(unsigned char *zm);
+// 获取下一个元素
 unsigned char *zipmapNext(unsigned char *zm, unsigned char **key, unsigned int *klen, unsigned char **value, unsigned int *vlen);
+// 通过key 获取value值
 int zipmapGet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char **value, unsigned int *vlen);
 int zipmapExists(unsigned char *zm, unsigned char *key, unsigned int klen);
+// 获取元素个数
 unsigned int zipmapLen(unsigned char *zm);
+// 获取整个zipmap占用的字节数，
 size_t zipmapBlobLen(unsigned char *zm);
 void zipmapRepr(unsigned char *p);
 
